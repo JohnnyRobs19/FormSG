42c527a62ca4863edcec37d21a37da44e629eb98
JohnnyRobs19 <jonathansiew.2004@gmail.com>
2025-08-06 17:41:49 +0800

    feat(i18n): add adminForbiddenError translation to taSG locale

--- frontend/src/i18n/locales/ta-sg.ts
+++ frontend/src/i18n/locales/ta-sg.ts
@@ -1,11 +1,13 @@
 import { PartialDeep } from 'type-fest'
 
+import { taSG as adminForbiddenError } from './features/admin-forbidden-error'
 import { taSG as publicForm } from './features/public-form'
 import Translation from './types'
 
 export const taSG: PartialDeep<Translation> = {
   translation: {
     features: {
+      adminForbiddenError,
       publicForm,
     },
   },
